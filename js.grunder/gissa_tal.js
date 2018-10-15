00a90b6f1c729311a83e3bcad0a2b774a51c2901
Carl-Axel Jirner <carlaxel.jirner@learnet.se>
2018-10-15 09:27:58 +0200

    förbättringar i vårt spell v2

--- js.grunder/gissa_tal.js
+++ js.grunder/gissa_tal.js
@@ -3,38 +3,49 @@ var slumptal = Math.ceil(Math.random() * 100);
 var elementGissning = document.querySelector('#gissning');
 var elementSvar = document.querySelector('#svar');
 console.log('Nytt slumptal: ' + slumptal);
-
+/* antal försök */
+var antal = 0;
 
 /* Reagera på när man trycker på knappen */
 function gissa() {
-     /* Läsa av gissningen i första input */
+
+    /* räkna upp */
+    antal++;
+
+    /* Läsa av gissningen i första input */
     var gissning = elementGissning.value;
     console.log('Gissningen: ' + gissning);
 
     /* Om gissning = slumptal då har man vinnit */
     if (gissning == slumptal) {
-        console.log('Yippee! Du har vinnit Äran!');
+        svar = '<p class="animated yipee">' + antal + ') ' + gissning + 'yippee! Du har vunnit äran!</p > ';
+        elementSvar.insertAdjacentHTML('beforeend', svar);
 
         /* skriv i andra input-rutan */
-        elementSvar.value = 'Yippee! Du har vinnit äran!'
+        elementSvar.value = 'Yippee! Du har vinnit äran!';
+
+        /* stäng av kappen */
+        document.querySelector('buttom').disabled = true;
+        /* en rolig effekt */
+        document.querySelector('.yipee').classList.add('fadeInDown');
     }
+
+
+
     /* Om gissningen > slumptal, skriv ut "för högt" */
     if (gissning > slumptal) {
-        console.log('För högt');
-         /* skriv i andra input-rutan */
-        elementSvar.value = 'för högt!';
-
+        svar = '<p class="hogt">' + antal + ') ' + gissning + ' för högt!</p>';
+        elementSvar.insertAdjacentHTML('beforeend', svar);
     }
     /* Om gissningen < slumptal, skriv ut "för lågt" */
     if (gissning < slumptal) {
-        console.log('För lågt');
-        /* skriv i andra input-rutan */
-        elementSvar.value = 'för Lågt!';
+        svar = '<p class="lagt">' + antal + ') ' + gissning + ' för lågt!</p>';
+        elementSvar.insertAdjacentHTML('beforeend', svar);
     }
     /* Om gissnig > 100, skriv ut "Är du du!" */
     if (gissning > 100) {
-        console.log('Är du dum!');
-        elementSvar.value = 'Är du dum!';
+        svar = '<p class="dum">' + antal + ') ' + gissning + ' är du dum!!</p>';
+        elementSvar.insertAdjacentHTML('beforeend', svar);
     }
 
 }
