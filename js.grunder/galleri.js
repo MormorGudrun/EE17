4386d9b39181351d3ddf517b53864180ba8a2eb2
Carl-Axel Jirner <carlaxel.jirner@learnet.se>
2018-10-25 13:29:24 +0200

    tränat på classer och qsele.

---
+++ js.grunder/galleri.js
@@ -0,0 +1,22 @@
+/* 
+* När man klickar på en knapp byts bilden ut till aktuell bild.
+*/
+
+/* När webbsidan laddats klart så kör start */
+window.onload = start();
+
+/* Start-delen */
+function start() {
+    
+
+    /* Vilka element arbetar vi med? */
+    const elementImg = document.querySelector('.bild img')
+    const elementP = document.querySelector('.bild p');
+    const elementKnapp1 = document.querySelector('.knapp1')
+    const elementKnapp2 = document.querySelector('.knapp2')
+    const elementKnapp3 = document.querySelector('.knapp3')
+    const elementKnapp4 = document.querySelector('.knapp4')
+    /* Vilka händelser behöver vi lyssna på? */
+
+    
+}
