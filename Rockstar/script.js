9c58ed0c9087faac7a986bd7a2584462bc9c3cee
Carl-Axel Jirner <carlaxel.jirner@learnet.se>
2019-03-18 09:35:25 +0100

    fortsat med hemsidan

---
+++ Rockstar/script.js
@@ -0,0 +1,5 @@
+$('.fullBackground').fullClip({
+  images: ['./bilder/coal.jpg', './bilder/rap-beats-rap-beats-1-1000x500.png', './bilder/annie-theby-1319641-unsplash.jpg'],
+  transitionTime: 2000,
+  wait: 5000
+});
